faae1b3e5e47fe43d1fa6188fe52c8d84b42526c
justarya <arya@mekari.com>
2021-06-19 20:39:08 +0700

    initial-setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from '@/pages/Home';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+};
+</script>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-home">
+    <div class="p-home__tools">
+      <button @click="addText">Text</button>
+    </div>
+    <canvas id="pdf-editor"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    addText() {},
+  },
+};
+</script>
+
+<style>
+
+</style>
